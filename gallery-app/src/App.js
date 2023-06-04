1c7076e382414304a2d5ac1b4efb4eb43bc16454
NBurtsev790231 <nburtsev7922@gmail.com>
2023-06-04 11:06:06 +0300

    new gallery app 04.06

---
+++ gallery-app/src/App.js
@@ -0,0 +1,17 @@
+/*
+import logo from './logo.svg';
+import './App.css';
+*/
+import '../styles/gallery.css';
+function App() {
+  return (
+    <section className="container">
+      <div className="card">
+        <img src="../img/RRS white.jpg"/>
+      </div>
+    </section>
+  )
+
+}
+
+export default App;
